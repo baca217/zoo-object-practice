05c96bac1408a0a1480cb2c422ef4711b4d0ac3e
Desktop PopOS <tieu5230@colorado.edu>
2020-09-08 20:10:27 -0600

    initial commit

---
+++ src/com/company/classes/Animal.java
@@ -0,0 +1,12 @@
+package com.company.classes;
+
+public abstract class Animal {
+    abstract public void wakeUp();
+    abstract public void makeNoise();
+    abstract public void eat();
+    abstract public void roam();
+    abstract public void sleep();
+    abstract public void printInfo();
+    abstract public void getName();
+    abstract public void setName();
+}
