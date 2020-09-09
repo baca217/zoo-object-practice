5d5b742c151d08d1a045f2e5d671b5f494908569
Elmer Baca Holguin <baca217@gmail.com>
2020-09-09 10:25:26 -0600

    added getter and setters for name in Animal class

--- src/main/java/com/example/demo/classes/Animal.java
+++ src/main/java/com/example/demo/classes/Animal.java
@@ -1,9 +1,12 @@
-package com.company.classes;
+package com.example.demo.classes;
 
 public abstract class Animal {
+    private String name;
     abstract public void wakeUp();
     abstract public void rollCall();
     abstract public void feed();
     abstract public void exercise();
     abstract public void sleep();
+    abstract public void getName();
+    abstract public void setName();
 }
