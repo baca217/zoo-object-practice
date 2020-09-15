43c534baaddebac36143059855c61e5d7fc34898
Elmer Baca Holguin <baca217@gmail.com>
2020-09-14 22:17:26 -0600

    made terminal output go to file in main

--- src/main/java/com/ZookeeperHomework1Application.java
+++ src/main/java/com/ZookeeperHomework1Application.java
@@ -4,13 +4,16 @@ import com.company.classes.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
 import java.util.Random;
 import java.util.Scanner;
 
 @SpringBootApplication
 public class ZookeeperHomework1Application {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		//SpringApplication.run(ZookeeperHomework1Application.class, args);
 		int days;
 		Scanner scan = new Scanner(System.in);
@@ -21,8 +24,12 @@ public class ZookeeperHomework1Application {
 		//Marsupial
 		Kangaroo a1 = new Kangaroo("Ken");
 		Wombat a2 = new Wombat("Wendy");
+		Kangaroo a3 = new Kangaroo("Kenny");
 		//Pachyderm
 		//
+		//https://stackoverflow.com/questions/1994255/how-to-write-console-output-to-a-txt-file
+		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+		System.setOut(out);
 		Animal[] list = new Animal[2];
 		list[0] = a1;
 		list[1] = a2;
